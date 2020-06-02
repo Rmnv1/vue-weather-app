7bd85e5e1c2e699df4b425fa49b27c9a65b68dad
Rmnv <rmnv.qqq@gmail.com>
2020-06-02 21:54:10 +0300

    Deploy app

---
+++ precache-manifest.98ea790b55f947639593ae9b614b21b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355284fab11955031091",
+    "url": "/vue-weather-app/css/app.e473540e.css"
+  },
+  {
+    "revision": "775ad4d166459c56f1d5ab6866d737c3",
+    "url": "/vue-weather-app/img/clouds.775ad4d1.svg"
+  },
+  {
+    "revision": "a81f0a400f3934c7d0d2dfbb791779b4",
+    "url": "/vue-weather-app/img/day.a81f0a40.jpg"
+  },
+  {
+    "revision": "a56a791585c9caa047aff2b838dfd086",
+    "url": "/vue-weather-app/img/rain.a56a7915.svg"
+  },
+  {
+    "revision": "0bb8cb16f4420944c265dfc314c09bb8",
+    "url": "/vue-weather-app/img/snow.0bb8cb16.svg"
+  },
+  {
+    "revision": "7e644c1a17230752ab109b8b5ebc6c02",
+    "url": "/vue-weather-app/img/thunderstorm.7e644c1a.svg"
+  },
+  {
+    "revision": "df622c7413ef22710e2f914c22356f32",
+    "url": "/vue-weather-app/index.html"
+  },
+  {
+    "revision": "355284fab11955031091",
+    "url": "/vue-weather-app/js/app.fcd62426.js"
+  },
+  {
+    "revision": "b4d3a10c06654cddabea",
+    "url": "/vue-weather-app/js/chunk-vendors.25f41afb.js"
+  },
+  {
+    "revision": "abb7651b3fb33cbb939069d3e6d6a7cc",
+    "url": "/vue-weather-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather-app/robots.txt"
+  }
+]);
